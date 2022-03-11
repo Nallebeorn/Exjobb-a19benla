ff5f31ff8b4a986cc76f2856a483f2ddab6a60b0
Benjamin <f.benjamin.larsson@gmail.com>
2022-03-11 16:17:09 +0100

    Screenshotting

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
@@ -10,7 +11,7 @@ public class CameraController : MonoBehaviour
     public float rotateSpeed = 1.0f;
 
     private bool pressedEscape = false;
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -30,9 +31,9 @@ public class CameraController : MonoBehaviour
         {
             pressedEscape = false;
         }
-        
+
         if (!Application.isFocused || pressedEscape) return;
-        
+
         float strafe = Input.GetAxis("Horizontal");
         float forward = Input.GetAxis("Vertical");
         float elevate = Input.GetAxis("Elevate");
@@ -41,13 +42,27 @@ public class CameraController : MonoBehaviour
         bool fast = Input.GetButton("Fire3");
 
         float delta = Time.deltaTime * (fast ? fastSpeed : speed);
-        
+
         transform.position += transform.forward * forward * delta;
         transform.position += transform.right * strafe * delta;
         transform.position += transform.up * elevate * delta;
-        
+
         transform.Rotate(Vector3.up, rotateSpeed * yaw, Space.World);
         transform.Rotate(Vector3.right, -rotateSpeed * pitch, Space.Self);
+        
+        ScreenshotSaving();
+    }
+
+    private void ScreenshotSaving()
+    {
+#if UNITY_EDITOR
+        if (Input.GetKeyDown(KeyCode.F12))
+        {
+            string filename = $"{Application.dataPath}/../Screenshots/Screenshot-{DateTime.Now:yyyy-MM-dd-HH-mm-ss}.png";
+            ScreenCapture.CaptureScreenshot(filename);
+            Debug.Log("Saved screenshot to: " + filename);
+        }
+#endif
     }
 
     private void OnApplicationFocus(bool hasFocus)
