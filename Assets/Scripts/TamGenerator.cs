85e6fd9e72a9b43e069d751b224fff9fb65e09b5
Benjamin <f.benjamin.larsson@gmail.com>
2022-03-25 15:52:44 +0100

    Mipmap test stroke

--- Assets/Scripts/TamGenerator.cs
+++ Assets/Scripts/TamGenerator.cs
@@ -1,10 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TamGenerator : MonoBehaviour
 {
-    public Texture2D texture { get; set; }
+    public Texture2D strokeTexture;
+    public Texture2D texture;
     public MeshRenderer[] textureTheseMeshes;
     
     private void Start()
@@ -23,20 +25,37 @@ public class TamGenerator : MonoBehaviour
     {
         texture = new Texture2D(2048, 2048, TextureFormat.RGBA32, true, true);
 
+        Color[] stroke = strokeTexture.GetPixels();
+        
         Color[][] tam = new Color[texture.mipmapCount][];
 
         int width = texture.width;
         int height = texture.height;
+        int strokeWidth = strokeTexture.width;
+        int strokeHeight = strokeTexture.height;
+        
         for (int m = 0; m < texture.mipmapCount; m++)
         {
             Color[] mip = new Color[width * height]; 
             tam[m] = mip;
-            Color color = Color.Lerp(Color.red, Color.blue, (float)m / (float)texture.mipmapCount);
+            // Color color = Color.Lerp(Color.red, Color.blue, (float)m / (float)texture.mipmapCount);
+            // for (int x = 0; x < width; x++)
+            // {
+            //     for (int y = 0; y < height; y++)
+            //     {
+            //         mip[y * width + x] = color;
+            //     }
+            // }
+
+            int offsetX = (strokeWidth - width) / 2;
+            int offsetY = (strokeHeight - height) / 2;
             for (int x = 0; x < width; x++)
             {
                 for (int y = 0; y < height; y++)
                 {
-                    mip[y * width + x] = color;
+                    int sourceX = offsetX + x;
+                    int sourceY = offsetY + y;
+                    mip[x + y * width] = stroke[sourceX + sourceY * strokeWidth];
                 }
             }
 
