01b399430d83015a0e29218bc69ccb48d9c64632
Benjamin <f.benjamin.larsson@gmail.com>
2022-04-01 14:24:22 +0200

    Mip generation progress

--- Assets/Scripts/TamGenerator.cs
+++ Assets/Scripts/TamGenerator.cs
@@ -2,12 +2,55 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class TamGenerator : MonoBehaviour
 {
     public Texture2D strokeTexture;
     public Texture2D texture;
     public MeshRenderer[] textureTheseMeshes;
+
+    public struct TextureColors
+    {
+        public int width;
+        public int height;
+        public Color32[] colors;
+        public readonly int mip;
+
+        public TextureColors(Texture2D texture, int mipLevel = 0)
+        {
+            width = texture.width >> mipLevel;
+            height = texture.height >> mipLevel;
+            colors = texture.GetPixels32(mipLevel);
+            mip = mipLevel;
+        }
+
+        public void SetPixel(int x, int y, Color32 color)
+        {
+            colors[x + y * width] = color;
+        }
+
+        public Color32 GetPixel(int x, int y)
+        {
+            return colors[x + y * width];
+        }
+
+        public void Fill(Color32 color)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    colors[x + y * width] = color;
+                }
+            }
+        }
+
+        public void WriteToTexture(Texture2D texture)
+        {
+            texture.SetPixels32(colors, mip);
+        }
+    }
 
     private void Start()
     {
@@ -24,67 +67,62 @@ public class TamGenerator : MonoBehaviour
     private void GenerateTextureMap()
     {
         texture = new Texture2D(2048, 2048, TextureFormat.RGBA32, true, true);
-        
-        Color[] stroke = strokeTexture.GetPixels();
 
-        Color[][] tam = new Color[texture.mipmapCount][];
-
-        int tamWidth = texture.width;
-        int tamHeight = texture.height;
-        int strokeWidth = strokeTexture.width;
-        int strokeHeight = strokeTexture.height;
+        TextureColors stroke = new TextureColors(strokeTexture);
+        TextureColors[] mips = new TextureColors[texture.mipmapCount];
 
         for (int m = 0; m < texture.mipmapCount; m++)
         {
-            Color[] mip = new Color[tamWidth * tamHeight];
-            tam[m] = mip;
-            // Color color = Color.Lerp(Color.red, Color.blue, (float)m / (float)texture.mipmapCount);
-            // for (int x = 0; x < width; x++)
-            // {
-            //     for (int y = 0; y < height; y++)
-            //     {
-            //         mip[y * width + x] = color;
-            //     }
-            // }
+            mips[m] = new TextureColors(texture, m);
+            mips[m].Fill(Color.white);
+        }
 
-            for (int i = 0; i < mip.Length; i++)
+        for (int m = texture.mipmapCount - 1; m >= 0; m--)
+        {
+            for (int i = 0; i < 5; i++)
             {
-                mip[i] = Color.white;
-            }
-
-            int offsetX = tamWidth / 2 - strokeWidth / 2;
-            int offsetY = tamHeight / 2 - strokeHeight / 2;
-            for (int x = 0; x < strokeWidth; x++)
-            {
-                for (int y = 0; y < strokeHeight; y++)
+                float s = Random.value;
+                float t = Random.value;
+                for (int mm = m; mm >= 0; mm--)
                 {
-                    Color strokePixel = stroke[x + y * strokeWidth];
-                    if (strokePixel.a > 0.001f)
-                    {
-                        int tamX = offsetX + x;
-                        int tamY = offsetY + y;
-                        if (tamX < 0 || tamX >= tamWidth || tamY < 0 || tamY >= tamHeight)
-                        {
-                            continue;
-                        }
-
-                        mip[tamX + tamY * tamWidth] = strokePixel;
-                    }
+                    BlitWrapped(new Vector2(s, t), mips[mm], stroke);
                 }
             }
-
-            tamWidth /= 2;
-            tamHeight /= 2;
         }
 
         for (int m = 0; m < texture.mipmapCount; m++)
         {
-            texture.SetPixels(tam[m], m);
+            mips[m].WriteToTexture(texture);
         }
-
+        
         texture.Apply(false);
 
         texture.filterMode = FilterMode.Trilinear;
         // texture.anisoLevel = 16;
+    }
+
+    static void BlitWrapped(Vector2 uv, TextureColors target, TextureColors image)
+    {
+        Vector2Int pixelCoords = new Vector2Int(Mathf.RoundToInt(uv.x * target.width), Mathf.RoundToInt(uv.y * target.height));
+        int offsetX = pixelCoords.x - image.width / 2;
+        int offsetY = pixelCoords.y - image.height / 2;
+
+        for (int imgx = 0; imgx < image.width; imgx++)
+        {
+            for (int imgy = 0; imgy < image.height; imgy++)
+            {
+                int targetX = Mod(offsetX + imgx, target.width);
+                int targetY = Mod(offsetY + imgy, target.height);
+                Color32 srcPixel = image.GetPixel(imgx, imgy);
+                Color32 dstPixel = target.GetPixel(targetX, targetY);
+                Color32 newColor = Color32.Lerp(dstPixel, srcPixel, srcPixel.a / 255.0f);
+                target.SetPixel(targetX, targetY, newColor);
+            }
+        }
+    }
+
+    static int Mod(int x, int modulo)
+    {
+        return (x % modulo + modulo) % modulo;
     }
 }
